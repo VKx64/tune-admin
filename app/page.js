3fff897f58377b000dc29f6bd0df9a98494cb16f
VKx64 <k-developer@zofky.com>
2025-07-25 15:13:53 +0800

    bayot billy

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
           Tune Admin
         </h1>
         <p className="mt-6 text-xl leading-8 text-gray-600">
-          Welcome to your Admin Assboard. Test CI/CD lol no Deploy
+          Welcome to your Admin Assboard. Test CI/CD lol no Deploy oten ni billy
         </p>
         <div className="mt-12 flex items-center justify-center gap-x-6">
           <Link
